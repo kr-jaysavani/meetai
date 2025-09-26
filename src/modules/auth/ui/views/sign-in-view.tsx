b698cc2058604590dcf1a2659ed9f82b170c1ef9
kr-jaysavani <jay.s@knackroot.com>
2025-09-26 10:53:38 +0530

    Add sign-up form with validation and error handling

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -116,7 +116,11 @@ export const SignInView = () => {
                     <AlertTitle>{error}</AlertTitle>
                   </Alert>
                 )}
-                <Button className="w-full" type="submit" disabled={loading}>
+                <Button
+                  className="w-full cursor-pointer"
+                  type="submit"
+                  disabled={loading}
+                >
                   Sign in
                 </Button>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
